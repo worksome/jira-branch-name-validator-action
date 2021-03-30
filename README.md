fc467420407e4911ffe6eaa672a681aac5896d8d
Oliver Nybroe <oliver@worksome.com>
2021-03-30 11:42:43 +0200

    git commit -m "Validate branch name"

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# jira-branch-name-validator-action
+# JIRA Branch Name Validation Action
 A GitHub action for validating if the branch name is contains JIRA id
+
+## Inputs
+
+### `branch-name`
+
+**Required** The name of the branch to validate against.
+
+## Example usage
+
+```
+uses: worksome/jira-branch-name-validator-action@master
+with:
+  branch-name: $BRANCH_NAME
+```
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core')
+
+let branchName = core.getInput("branch-name")
+core.info(`Received the following branch name ${branchName}.`)
+
+if (!branchName.startsWith('JIRA')) {
+    core.setFailed(`Branch doesn't start with \`JIRA\` prefix, found ${branchName}.`)
+    return
+}
+
+branchName = branchName.substring(4)
+if (!branchName.startsWith('-')) {
+    core.setFailed(`Separator after prefix is not \`-\`, found ${branchName.substring(0, 1)}.`)
+    return
+}
+
+branchName = branchName.substring(1);
+const rawJiraId = branchName.match(/^\d*/)[0]
+const jiraId = parseInt(rawJiraId)
+if (isNaN(jiraId) || jiraId === 0 || rawJiraId.length === jiraId.toString().length) {
+    core.setFailed(`JIRA id is not a valid number, found ${rawJiraId}.`)
+    return
+}
+
+branchName = branchName.substring(rawJiraId.length)
+if (!branchName.startsWith('-')) {
+    core.setFailed(`Separator after JIRA id is not \`-\`, found ${branchName.substring(0, 1)}.`)
+    return
+}
+
+branchName = branchName.substring(1)
+if (/^[a-zA-Z0-9_]+$/.test(branchName)) {
+    core.setFailed(`Description after JIRA id should use underscore as word separator, found ${branchName}.`)
+}

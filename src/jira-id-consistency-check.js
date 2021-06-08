ad656a7204c7b0921c65b2573e4b5db7ce515dcb
Bogdan Sorlea <bogdan@worksome.com>
2021-06-08 13:15:17 +0300

    fix(ci): JIRA-2085 improve Jira ID consistency validation
    Improved the check as to have a 3-way matching of the Jira ID,
    to ensure it's the same in branch name, PR title and commits.

---
+++ src/jira-id-consistency-check.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core')
+
+export default function(jiraId, prTitle, commits) {
+
+    let result = []
+
+    if (prTitle.search(jiraId) < 0) {
+        result.push(`PR title <${prTitle}> does not contain Jira ID inferred from branch name, ${jiraId}`)
+    }
+
+    JSON.parse(commits).filter(c => c.commit.message.search(jiraId) < 0).forEach(c => {
+        result.push(`Commit message <${c.commit.message}> does not contain Jira ID inferred from branch name, ${jiraId}`)
+    })
+    core.info(`PR and commit validation errors: ${result.length}`)
+    return result
+}
+

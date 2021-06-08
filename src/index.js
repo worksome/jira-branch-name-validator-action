8ec205253b7e1428e096100f20b0c021b03978a0
Bogdan Sorlea <bogdan@worksome.com>
2021-06-08 13:15:17 +0300

    fix(ci): JIRA-2085 improve Jira ID consistency validation
    Improved the check as to have a 3-way matching of the Jira ID,
    to ensure it's the same in branch name, PR title and commits.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import validateBranchName from './validator';
 const core = require('@actions/core')
+const github = require('@actions/github');
 
 let branchName = core.getInput("branch-name")
-core.info(`Received the following branch name ${branchName}.`)
+let prTitle = core.getInput("pr-title")
+let commits = core.getInput("commits")
+let event = core.getInput("event")
+core.info("Event:")
+core.info(JSON.stringify(event, undefined, 2))
+core.info("Github:")
+core.info(JSON.stringify(github, undefined, 2))
+core.info("Event via module:")
+core.info(JSON.stringify(github.event, undefined, 2))
+core.info(`Received the following branch name: ${branchName}.`)
 core.info("The format should be `JIRA-123_fixing-bug`.")
+core.info(`Received the following PR title: ${prTitle}`)
+core.info("Should contain the same JIRA ID.")
+core.info("Received the following commits information:")
+core.info(commits)
+core.info("Commit message should contain the same JIRA ID as above.")
 
-const results = validateBranchName(branchName)
+const payload = JSON.stringify(github.context.payload, undefined, 2)
+console.log(`The event payload:`);
+console.log(`${payload}`);
+
+const results = validateBranchName(branchName, prTitle, commits)
 
 results.forEach(message => {
     core.setFailed(message)
